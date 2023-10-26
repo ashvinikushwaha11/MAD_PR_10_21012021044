029291523ab68b6cce7a17f9e1f991ccc27b704a
Admin <ashvinikushwaha189@gmail.com>
2023-10-27 04:44:47 +0530

    okk

--- app/src/main/java/com/example/mad_pr_10_21012021044/MainActivity.kt
+++ app/src/main/java/com/example/mad_pr_10_21012021044/MainActivity.kt
@@ -1,11 +1,75 @@
 package com.example.mad_pr_10_21012021044
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var personListView:ListView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        personListView=findViewById(R.id.listview_main)
+
+        val btn_flt:FloatingActionButton=findViewById(R.id.floating_btn)
+        btn_flt.setOnClickListener{
+            setPersonDataToListView()
+            Intent(MainActivity@this,MapsActivity::class.java).also { startActivity(it) }
+        }
+    }
+    fun setPersonDataToListView(){
+
+        //personListView.adapter=PersonAdapter(this, arrayListOf(
+           // Person("01","Ash","ash@email.com","9568472103","ahemedabad", 23.0225 , 72.5714),
+           // Person("02","Krup","krup@email.com","9568472104","my home", 23.0225 , 72.5714),
+            //Person("03","Rutz","rutz@email.com","9568472105","ahemedabad", 23.0225 , 72.5714),
+            //Person("04","Kru","kru@email.com","9568472106","ahemedabad", 23.0225 , 72.5714),
+            //Person("05","Vish","vish@email.com","9568472107","ahemedabad", 23.0225 , 72.5714),
+            //Person("06","Sujz","suj@email.com","9568472108","ahemedabad", 23.0225 , 72.5714),
+        //))
+
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val data = HttpRequest().makeServiceCall(
+                    "https://api.json-generator.com/templates/qjeKFdjkXCdK/data",
+                    "anckf6ysyjuvv9s2hn6yvtm1p55a7ol11j03ze7l")
+                withContext(Dispatchers.Main) {
+                    try {
+                        if(data != null)
+                            runOnUiThread{getPersonDetailsFromJson(data)}
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                }
+            }
+            catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun getPersonDetailsFromJson(sJson: String?) {
+        val personList = ArrayList<Person>()
+        try {
+            val jsonArray = JSONArray(sJson)
+            for (i in 0 until jsonArray.length()) {
+                val jsonObject = jsonArray[i] as JSONObject
+                val person = Person(jsonObject)
+                personList.add(person)
+            }
+            var listView1 : ListView = findViewById(R.id.listview_main)
+            listView1.adapter = PersonAdapter(this, personList)
+        } catch (ee: JSONException) {
+            ee.printStackTrace()
+        }
     }
 }
--- app/src/main/java/com/example/mad_pr_10_21012021044/PersonAdapter.kt
+++ app/src/main/java/com/example/mad_pr_10_21012021044/PersonAdapter.kt
@@ -5,11 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import java.util.ArrayList
 
 class PersonAdapter(context: Context, val array:ArrayList<Person>):ArrayAdapter<Person>(context,0, array) {
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
         val view=LayoutInflater.from(parent.context).inflate(R.layout.person_item, parent,false)
+        val persondata=array.get(position)
+        view.findViewById<TextView>(R.id.p_name1).text=persondata.name
+        view.findViewById<TextView>(R.id.p1_email).text=persondata.emailId
+        view.findViewById<TextView>(R.id.p1_add).text=persondata.address
+        view.findViewById<TextView>(R.id.p1_num).text=persondata.phoneNo
         return view
     }
 }
